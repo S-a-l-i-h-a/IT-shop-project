b1dc02ccf4211641ffd8d42373371a61588e0b57
Saliha <90252317+S-a-l-i-h-a@users.noreply.github.com>
2022-04-13 10:23:32 +0200

    services

--- rest/services/CustomerService.class.php
+++ rest/services/CustomerService.class.php
@@ -1,12 +1,12 @@
 <?php
 require_once __DIR__.'/BaseService.class.php';
-require_once __DIR__.'/../dao/ProjectDao.class.php';
+require_once __DIR__.'/../dao/CustomerDao.class.php';
 
 class CustomerService extends BaseService{
 
   public function __construct(){
-    parent::__construct(new ProjectDao());
+    parent::__construct(new CustomerDao());
   }
 
-
+}
 ?>
--- rest/services/ProductService.class.php
+++ rest/services/ProductService.class.php
@@ -1,12 +1,13 @@
 <?php
 require_once __DIR__.'/BaseService.class.php';
-require_once __DIR__.'/../dao/ProjectDao.class.php';
+require_once __DIR__.'/../dao/ProductDao.class.php';
 
 class ProductService extends BaseService{
 
   public function __construct(){
-    parent::__construct(new ProjectDao());
+    parent::__construct(new ProductDao());
   }
 
+}
 
 ?>
