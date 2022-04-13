6a4d9432a5a4ded909d65087e2c29b831a043cfc
Saliha <90252317+S-a-l-i-h-a@users.noreply.github.com>
2022-04-13 10:22:45 +0200

    customer routes

--- rest/routes/CustomerRoutes.php
+++ rest/routes/CustomerRoutes.php
@@ -5,21 +5,21 @@
 * List all todos
 */
 Flight::route('GET /Customers', function(){
-  Flight::json(Flight::CustomerService()->get_all());
+  Flight::json(Flight::customerService()->get_all());
 });
 
 /**
 * List invidiual todo
 */
 Flight::route('GET /Customers/@id', function($id){
-  Flight::json(Flight::CustomerService()->get_by_id($id));
+  Flight::json(Flight::customerService()->get_by_id($id));
 });
 
 /**
 * add todo
 */
 Flight::route('POST /Customers', function(){
-  Flight::json(Flight::CustomerService()->add(Flight::request()->data->getData()));
+  Flight::json(Flight::customerService()->add(Flight::request()->data->getData()));
 });
 
 /**
@@ -27,14 +27,14 @@ Flight::route('POST /Customers', function(){
 */
 Flight::route('PUT /Customers/@id', function($id){
   $data = Flight::request()->data->getData();
-  Flight::json(Flight::CustomerService()->update($id, $data));
+  Flight::json(Flight::customerService()->update($id, $data));
 });
 
 /**
 * delete todo
 */
 Flight::route('DELETE /Customers/@id', function($id){
-  Flight::CustomerService()->delete($id);
+  Flight::customerService()->delete($id);
   Flight::json(["message" => "deleted"]);
 });
 
