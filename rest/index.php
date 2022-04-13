2744c26c3e63fb33c1e9aa3826651f5f26cac9af
Saliha <90252317+S-a-l-i-h-a@users.noreply.github.com>
2022-04-13 10:23:48 +0200

    commit

--- rest/index.php
+++ rest/index.php
@@ -5,10 +5,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require '../vendor/autoload.php';
-require_once 'dao/ProjectDao.class.php';
+require_once 'dao/BaseDao.class.php';
 
-Flight::route('/', function (){
-  echo 'Hello Worlddddd';
-});
+require_once 'services/CustomerService.class.php';
+require_once 'services/ProductService.class.php';
 
-Flight::register('ProjectDao', 'ProjectDao');
+
+
+Flight::register('customerService', 'CustomerService');
+Flight::register('productService', 'ProductService');
+
+
+require_once 'routes/CustomerRoutes.php';
+require_once 'routes/ProductRoutes.php';
+
+
+
+Flight::start();
+?>
